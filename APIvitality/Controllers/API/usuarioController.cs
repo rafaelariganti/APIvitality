49a518265e194aad2a41eb7df458ca60446bcff1
rafariganti <rafaelariganti@gmail.com>
2024-09-19 17:58:32 -0300

    Adicionar arquivos de projeto.

---
+++ APIvitality/Controllers/API/usuarioController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APIvitality.Models;
+
+namespace APIvitality.Controllers.API
+{
+    public class usuarioController : ApiController
+    {
+        private vitality db = new vitality();
+
+        // GET: api/usuario
+        public IQueryable<usuarios> Getusuarios()
+        {
+            return db.usuarios;
+        }
+
+        // GET: api/usuario/5
+        [ResponseType(typeof(usuarios))]
+        public IHttpActionResult Getusuarios(int id)
+        {
+            usuarios usuarios = db.usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuarios);
+        }
+
+        // PUT: api/usuario/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putusuarios(int id, usuarios usuarios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuarios.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuarios).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!usuariosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/usuario
+        [ResponseType(typeof(usuarios))]
+        public IHttpActionResult Postusuarios(usuarios usuarios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.usuarios.Add(usuarios);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = usuarios.id }, usuarios);
+        }
+
+        // DELETE: api/usuario/5
+        [ResponseType(typeof(usuarios))]
+        public IHttpActionResult Deleteusuarios(int id)
+        {
+            usuarios usuarios = db.usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            db.usuarios.Remove(usuarios);
+            db.SaveChanges();
+
+            return Ok(usuarios);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool usuariosExists(int id)
+        {
+            return db.usuarios.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ APIvitality/Controllers/usuarioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using APIvitality.Models;
+
+namespace APIvitality.Controllers
+{
+    public class usuarioController : Controller
+    {
+        private vitality db = new vitality();
+
+        // GET: usuario
+        public ActionResult Index()
+        {
+            return View(db.usuarios.ToList());
+        }
+
+        // GET: usuario/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = db.usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: usuario/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nome_usuario,email,senha")] usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        // GET: usuario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = db.usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: usuario/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nome_usuario,email,senha")] usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuarios);
+        }
+
+        // GET: usuario/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = db.usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            usuarios usuarios = db.usuarios.Find(id);
+            db.usuarios.Remove(usuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
